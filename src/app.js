f768bbb404c055fc2d705fa561fbeb4c118b728f
DHSLopes <daniel.h.s.lopes@gmail.com>
2024-07-04 19:52:01 -0300

    versao 1.0 finalizada

--- src/app.js
+++ src/app.js
@@ -1,43 +1,6 @@
-function criptografarTexto() {
-    let textoDigitado = document.getElementById('txtTextoDigitado').value;
-    textoDigitado = textoDigitado.toLowerCase();
-    textoDigitado = textoDigitado.replace(/e/g, "enter");
-    textoDigitado = textoDigitado.replace(/i/g, "imes");
-    textoDigitado = textoDigitado.replace(/a/g, "ai");
-    textoDigitado = textoDigitado.replace(/o/g, "ober");
-    textoDigitado = textoDigitado.replace(/u/g, "ufat");
-    manipulaImagem();
-    manipulaMensagens(textoDigitado);
-}
-
-function descriptografarTexto() {
-    let textoDigitado = document.getElementById('txtTextoDigitado').value;
-    textoDigitado = textoDigitado.toLowerCase();
-    textoDigitado = textoDigitado.replace(/enter/g, "e");
-    textoDigitado = textoDigitado.replace(/imes/g, "i");
-    textoDigitado = textoDigitado.replace(/ai/g, "a");
-    textoDigitado = textoDigitado.replace(/ober/g, "o");
-    textoDigitado = textoDigitado.replace(/ufat/g, "u");
-    manipulaImagem();
-    manipulaMensagens(textoDigitado);
-}
-
-function copiarTexto(){
-    let textoParaCopiar = document.getElementById('txtTextoGerado').value;
-    let span = document.getElementById('spanCopiadoComSucesso');
-    navigator.clipboard.writeText(textoParaCopiar);
-    span.style.display = 'block';
-    setTimeout(function() {
-        span.style.display = 'none';
-    }, 3000);
-}
-
 function manipulaImagem() {
     let imagem = document.getElementById('imgImagem');
-    if(imagem.style.display == 'none'){
-        imagem.style.display = 'block';
-    }
-    else{
+    if(imagem.style.display == 'flex'){
         imagem.style.display = 'none';
     }
 }
@@ -47,15 +10,61 @@ function manipulaMensagens(msg) {
     let nenhuma = document.getElementById('pNenhumaMensagem');
     let botaoCopiar = document.getElementById('btnCopiar');
     if(texto.style.display == 'none'){
-        texto.style.display = 'block';
-        texto.value += msg + "\n";
-        nenhuma.style.display = 'none';
-        botaoCopiar.style.display = 'block';
+            texto.style.display = 'block';
+            texto.value += msg;
+            nenhuma.style.display = 'none';
+            botaoCopiar.style.display = 'block';
     }
     else{
-        texto.style.display = 'none';
-        texto.value = "";
-        nenhuma.style.display = 'block';
-        botaoCopiar.style.display = 'none';
+        texto.value = msg;
     }
+}
+
+function limpaMensagemGerada(){
+    let texto = document.getElementById('txtTextoGerado');
+    texto.value = "";
+}
+
+function limpaMensagem(){
+    let texto = document.getElementById('txtTextoDigitado');
+    texto.value = "";
+}
+
+function criptografarTexto() {
+    limpaMensagemGerada();
+    let textoDigitado = document.getElementById('txtTextoDigitado').value;
+    textoDigitado = textoDigitado.toLowerCase();
+    textoDigitado = textoDigitado.replace(/e/g, "enter");
+    textoDigitado = textoDigitado.replace(/i/g, "imes");
+    textoDigitado = textoDigitado.replace(/a/g, "ai");
+    textoDigitado = textoDigitado.replace(/o/g, "ober");
+    textoDigitado = textoDigitado.replace(/u/g, "ufat");
+    manipulaImagem();
+    manipulaMensagens(textoDigitado);
+    limpaMensagem();
+}
+
+function descriptografarTexto() {
+    limpaMensagemGerada();
+    let textoDigitado = document.getElementById('txtTextoDigitado').value;
+    textoDigitado = textoDigitado.toLowerCase();
+    textoDigitado = textoDigitado.replace(/enter/g, "e");
+    textoDigitado = textoDigitado.replace(/imes/g, "i");
+    textoDigitado = textoDigitado.replace(/ai/g, "a");
+    textoDigitado = textoDigitado.replace(/ober/g, "o");
+    textoDigitado = textoDigitado.replace(/ufat/g, "u");
+    manipulaImagem();
+    manipulaMensagens(textoDigitado);
+    limpaMensagem();
+}
+
+function copiarTexto(){
+    let textoParaCopiar = document.getElementById('txtTextoGerado').value;
+    let span = document.getElementById('spanCopiadoComSucesso');
+    navigator.clipboard.writeText(textoParaCopiar);
+    limpaMensagemGerada();
+    span.style.display = 'block';
+    setTimeout(function() {
+        span.style.display = 'none';
+    }, 3000);
 }
