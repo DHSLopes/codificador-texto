f7ac9def203d1f6c7b75deeeda218b67d2b56bb2
DHSLopes <daniel.h.s.lopes@gmail.com>
2024-07-05 00:01:39 -0300

    Update versão 1.1 - entrega inicial do desafio

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function validaEntrada(){
+    let mensagem = document.getElementById('txtTextoDigitado').value;
+    for (var i = 0; i < mensagem.length; i++) {
+        var charCode = mensagem.charCodeAt(i);
+        console.log(mensagem[i])
+        if ((charCode >= 33 && charCode <= 43) || /* CARACTERES ESPECIAIS, NUMEROS E LETRAS MAIÚSCULAS */
+            (charCode == 45) || /* ESSA LINHA ADICIONA O HÍFEN, MAS REMOVE VÍRGULAS E PONTOS DO TESTE */
+            (charCode >= 47 && charCode <= 96) || /* OUTROS CARACTERES ESPECIAIS, NÚMEROS E LETRAS MAIÚSCULAS */
+            (charCode >= 123 && charCode <= 255)) {
+                return false;
+            }
+    }
+    return true;
+}
+
 function manipulaImagem() {
     let imagem = document.getElementById('imgImagem');
     if(imagem.style.display == 'flex'){
@@ -31,31 +46,42 @@ function limpaMensagem(){
 }
 
 function criptografarTexto() {
-    limpaMensagemGerada();
-    let textoDigitado = document.getElementById('txtTextoDigitado').value;
-    textoDigitado = textoDigitado.toLowerCase();
-    textoDigitado = textoDigitado.replace(/e/g, "enter");
-    textoDigitado = textoDigitado.replace(/i/g, "imes");
-    textoDigitado = textoDigitado.replace(/a/g, "ai");
-    textoDigitado = textoDigitado.replace(/o/g, "ober");
-    textoDigitado = textoDigitado.replace(/u/g, "ufat");
-    manipulaImagem();
-    manipulaMensagens(textoDigitado);
-    limpaMensagem();
+    if(validaEntrada()){
+        limpaMensagemGerada();
+        let textoDigitado = document.getElementById('txtTextoDigitado').value;
+        textoDigitado = textoDigitado.replace(/e/g, "enter");
+        textoDigitado = textoDigitado.replace(/i/g, "imes");
+        textoDigitado = textoDigitado.replace(/a/g, "ai");
+        textoDigitado = textoDigitado.replace(/o/g, "ober");
+        textoDigitado = textoDigitado.replace(/u/g, "ufat");
+        manipulaImagem();
+        manipulaMensagens(textoDigitado);
+        limpaMensagem();
+    }
+    else{
+        alert('O texto não pode conter letras maiúsculas, números ou caracteres especiais.\nFavor, reescreva a mensagem!');
+        limpaMensagem();
+    }
 }
 
 function descriptografarTexto() {
-    limpaMensagemGerada();
-    let textoDigitado = document.getElementById('txtTextoDigitado').value;
-    textoDigitado = textoDigitado.toLowerCase();
-    textoDigitado = textoDigitado.replace(/enter/g, "e");
-    textoDigitado = textoDigitado.replace(/imes/g, "i");
-    textoDigitado = textoDigitado.replace(/ai/g, "a");
-    textoDigitado = textoDigitado.replace(/ober/g, "o");
-    textoDigitado = textoDigitado.replace(/ufat/g, "u");
-    manipulaImagem();
-    manipulaMensagens(textoDigitado);
-    limpaMensagem();
+    if(validaEntrada()){
+        limpaMensagemGerada();
+        let textoDigitado = document.getElementById('txtTextoDigitado').value;
+        textoDigitado = textoDigitado.toLowerCase();
+        textoDigitado = textoDigitado.replace(/enter/g, "e");
+        textoDigitado = textoDigitado.replace(/imes/g, "i");
+        textoDigitado = textoDigitado.replace(/ai/g, "a");
+        textoDigitado = textoDigitado.replace(/ober/g, "o");
+        textoDigitado = textoDigitado.replace(/ufat/g, "u");
+        manipulaImagem();
+        manipulaMensagens(textoDigitado);
+        limpaMensagem();
+    }
+    else{
+        alert('O texto não pode conter letras maiúsculas, números ou caracteres especiais.\nFavor, reescreva a mensagem!');
+        limpaMensagem();
+    }
 }
 
 function copiarTexto(){
